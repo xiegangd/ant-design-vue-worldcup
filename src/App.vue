45586561050fb5624ea180a21d103c8746f8f3d3
xiegangd <xiegangd@gmail.com>
2018-08-12 11:03:38 +0800

    清楚自动生成的代码，页面只保留一个按钮

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <a-button type="primary">Button</a-button>
     <HelloWorld/>
   </div>
@@ -19,11 +18,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
