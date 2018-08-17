526c340ccc07c382edf214bc4b693f14afe4e739
xiegangd <xiegangd@gmail.com>
2018-08-17 22:08:42 +0800

    多语言切换

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,7 +4,7 @@
       <a-checkbox @change="onChange">显示红牌</a-checkbox>
       <a-checkbox @change="onChange">显示黄牌</a-checkbox>
 
-      <a-radio-group defaultValue=0>
+      <a-radio-group defaultValue=0 v-model="lang">
         <a-radio-button value=0>简体</a-radio-button>
         <a-radio-button value=1>繁体</a-radio-button>
         <a-radio-button value=2>英文</a-radio-button>
@@ -12,6 +12,9 @@
     </div>
 
     <a-table :columns="columns" :dataSource="data">
+      <template slot="league" slot-scope="league">{{ league[lang] }}</template>
+      <template slot="home" slot-scope="home">{{ home[lang] }}</template>
+      <template slot="guest" slot-scope="guest">{{ guest[lang] }}</template>
       <span slot="score" slot-scope="text, record, index">{{ record.homeScore }} - {{ record.guestScore}}</span>
       <span slot="homeScore" slot-scope="text, record, index">{{ record.homeHalfScore }} - {{ record.guestHalfScore}}</span>
     </a-table>
@@ -24,20 +27,23 @@
 
   const columns = [{
     title: '赛事',
-    dataIndex: 'league[0]',
+    dataIndex: 'league',
+    scopedSlots: { customRender: 'league' },
   }, {
     title: '时间',
     dataIndex: 'matchTime',
   }, {
     title: '主队',
-    dataIndex: 'home[0]',
+    dataIndex: 'home',
+    scopedSlots: { customRender: 'home' },
   }, {
     title: '全场比分',
     dataIndex: 'score',
     scopedSlots: { customRender: 'score' },
   }, {
     title: '客队',
-    dataIndex: 'guest[0]',
+    dataIndex: 'guest',
+    scopedSlots: { customRender: 'guest' },
   }, {
     title: '半场比分',
     dataIndex: 'homeScore',
@@ -53,6 +59,7 @@ export default {
     return {
       data: [],
       columns,
+      lang: 0,
     }
   },
   methods: {
